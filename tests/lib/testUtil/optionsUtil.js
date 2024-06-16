492a5e49ccbf486099e8577d5f82fc186123026d
AndreaPontrandolfo <andrea.pontra@gmail.com>
2024-06-16 23:25:39 +0200

    refactoring: removed callback binding

--- tests/lib/testUtil/optionsUtil.js
+++ tests/lib/testUtil/optionsUtil.js
@@ -1,25 +1,25 @@
-'use strict'
-const _ = require('lodash')
+"use strict";
+const _ = require("lodash");
 
 function fromMessage(message) {
-    return fromOptions({errors: [{message}]})
+  return fromOptions({ errors: [{ message }] });
 }
 
 function fromMessageId(messageId) {
-    return fromOptions({errors: [{messageId}]})
+  return fromOptions({ errors: [{ messageId }] });
 }
 
 function fromOptions(options) {
-    return function (testCase) {
-        return _.isString(testCase) ? _.assign({code: testCase}, options) : _.defaultsDeep(testCase, options)
-    }
+  return function (testCase) {
+    return _.isString(testCase)
+      ? _.assign({ code: testCase }, options)
+      : _.defaultsDeep(testCase, options);
+  };
 }
 
 module.exports = {
-    fromMessage,
-    fromMessageId,
-    fromVersion3: fromOptions({settings: {lodash: {version: 3}}}),
-    fromVersion3WithDefaultPragma: fromOptions({settings: {lodash: {version: 3, pragma: '_'}}}),
-    fromOptions,
-    withDefaultPragma: fromOptions({settings: {lodash: {pragma: '_'}}})
-}
+  fromMessage,
+  fromMessageId,
+  fromOptions,
+  withDefaultPragma: fromOptions({ settings: { lodash: { pragma: "_" } } }),
+};
