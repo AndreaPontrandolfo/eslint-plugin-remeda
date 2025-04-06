2080575c030e86c1389b2fa6b9d880f3ad383c7c
AndreaPontrandolfo <andrea.pontra@gmail.com>
2025-04-06 18:38:08 +0200

    refactor: improve collection method value rule with message IDs for better localization

--- src/rules/collection-method-value.ts
+++ src/rules/collection-method-value.ts
@@ -1,5 +1,5 @@
 /**
- * @file Rule to enforce usage of collection method values.
+ * Rule to enforce usage of collection method values.
  */
 
 import { includes } from "lodash-es";
@@ -16,34 +16,37 @@ const { getMethodName } = astUtil;
 const meta = {
   type: "problem",
   schema: [],
+  messages: {
+    useReturnValue: "Use value returned from R.{{method}}",
+    dontUseReturnValue: "Do not use value returned from R.{{method}}",
+  },
   docs: {
     description: "Use value returned from collection methods properly",
     url: getDocsUrl("collection-method-value"),
   },
 } as const;
 
+function parentUsesValue(node) {
+  return node.parent.type !== "ExpressionStatement";
+}
+
+function isSideEffectIterationMethod(method) {
+  return includes(getSideEffectIterationMethods(), method);
+}
+
+function isParentCommit(node, callType) {
+  return callType === "chained" && isCallToMethod(node.parent.parent, "commit");
+}
+
 function create(context) {
-  function parentUsesValue(node) {
-    return node.parent.type !== "ExpressionStatement";
-  }
-
-  function isSideEffectIterationMethod(method) {
-    return includes(getSideEffectIterationMethods(), method);
-  }
-
-  function isParentCommit(node, callType) {
-    return (
-      callType === "chained" && isCallToMethod(node.parent.parent, "commit")
-    );
-  }
-
   return getRemedaMethodVisitors(
     context,
     (node, iteratee, { method, callType }) => {
       if (isCollectionMethod(method) && !parentUsesValue(node)) {
         context.report({
           node,
-          message: `Use value returned from R.${method}`,
+          messageId: "useReturnValue",
+          data: { method },
         });
       } else if (
         isSideEffectIterationMethod(method) &&
@@ -52,7 +55,8 @@ function create(context) {
       ) {
         context.report({
           node,
-          message: `Do not use value returned from R.${getMethodName(node)}`,
+          messageId: "dontUseReturnValue",
+          data: { method: getMethodName(node) },
         });
       }
     },
