0832af3a05f9ececfe12929a007a734f24572d0e
AndreaPontrandolfo <andrea.pontra@gmail.com>
2025-05-24 18:45:42 +0200

    removed `skipNodeModulesBundle` and `clean` options. Fixes #28

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,8 +3,6 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
   entry: ["./src/index.ts"],
   format: ["esm", "cjs"],
-  clean: true,
-  skipNodeModulesBundle: true,
   shims: true,
   publint: { strict: true },
   dts: true,
